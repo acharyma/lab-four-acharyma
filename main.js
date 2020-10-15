b82db899d663c94cd7c9cc88d87cf9e9a55a6786
Manogya Acharya <acharyma@bc.edu>
2020-10-14 20:13:52 -0400

    GitHub Pages Set

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 d3.csv('wealth-health-2014.csv', d3.autoType).then(data=>{
+    //GITHUB PAGES FIX
     
     console.log('Wealth Health', data);
 
     const margin = ({top: 20, right: 20, bottom: 20, left: 20});
     const width = 650 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
+    
 
 
     const svg = d3.select(".chart").append("svg").attr("width", width + margin.left + margin.right) .attr("height", height + margin.top + margin.bottom) .append("g") .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
